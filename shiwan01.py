6849bde8fe57fb14e058844ac2a95615263f6a9a
theicerose <theicerose@sina.com>
2020-09-21 20:09:36 +0800

    1111

--- shiwan01.py
+++ shiwan01.py
@@ -34,18 +34,43 @@ print(a)
 # len 支持字符串、元祖、数组、字典
 # a="dfghjkiuytfghjtfghjiuy"
 # print(len(a))
-a=input("输入内容1:")
-b=input("输入内容2:")
-print(len(a+b))
+# a=input("输入内容1:")
+# b=input("输入内容2:")
+# print(len(a+b))
 
-print("试玩")   # 字符串
-print(2333)     # 整数
-print(2.3)      #小数
-print(False)    #布尔值 Ture or false
-print(())       #元祖
-print([])       #数组
-print({})       #字典
-print("试玩"+"01")    #字符串拼接
-print("试玩"*3)
-print(1+2,3/2,4*5)    #数学运算
-print(1+1==2,2>3) 
+# print("试玩")   # 字符串
+# print(2333)     # 整数
+# print(2.3)      #小数
+# print(False)    #布尔值 Ture or false Ture=1,False=0
+# print(())       #元祖
+# print([])       #数组
+# print({})       #字典
+# print("试玩"+"01")    #字符串拼接
+# print("试玩"*3)
+# print(1+2,3/2,4*5)    #数学运算
+# print(1+1==2,2>3) 
+
+#元祖  下标，从0开始编号
+# a=(2,34,67,8,2348,3,5,7,34,6,2,577,'来玩哟','骚年',True,False)
+# b=(a,'骚年','骚年',57)
+# print(a),print(a[4])
+#print(a.index(2))
+# index 搜索从左往右第一个数据的下标
+# print(a.count(2)) count 计算元祖里有几个2
+#print(b[0][4])   #层层递进，一层层下标定位
+
+#切片
+#print(a[0:6])   #左闭右开，从0到第5个
+#print(a[7:])
+
+#元祖写好后不可修改，数组是可以修改的
+#数组
+a=[2,34,67,8,2348,3,5,7,34,6,2,577,'来玩哟','骚年',True,False]
+#a.append('小意思')  append 往数组中追加数据（在最尾部）
+#a.insert(0,'开头')   insert 往数组中指定位置插入数据,0是下标位置
+# a.pop(-4)            #   pop 剪切（-4是下标位置）剪切后原数组就失去该数据
+# b=a.pop(-4)          #pop剪切出去的可以给其他变量赋值
+# a.clear()       clear 清空
+# xx=['完蛋',123,1]         
+# a.extend(xx)             extend可以加入数组，用a+xx也能实现
+print(a)
